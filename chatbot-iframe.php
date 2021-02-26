2e2588d81e4839eaf2b96b2acc6ed7c22a7feee0
Scott Tirrell <stirrell@second-cup-of-coffee.com>
2021-02-26 16:17:14 -0500

    Fixed iFrame link to the CSS file.

--- chatbot-iframe.php
+++ chatbot-iframe.php
@@ -3,7 +3,7 @@
 <head>
     <title>BotMan Widget</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="dist/css/style.css">
+    <link rel="stylesheet" type="text/css" href="dist/css/styles.css">
 </head>
 <body>
 	<script id="botmanWidget" src='dist/js/chat-min.js'></script>
